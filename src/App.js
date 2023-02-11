61741a976b2b5f24724721992a20692f74e4f188
Isha Lohia <ishalohia@Ishas-MacBook-Air.local>
2023-02-11 13:04:08 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React,{useState} from 'react'
+import MemberTable from './componets/MemberTable'
+import FormInput from "./componets/FormInput";
+import { Routes,Route} from "react-router-dom";
 function App() {
+ 
+  const [members,setmembers]=useState([
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+  
+      <Route path="/" element={<FormInput members={members
+      } setmembers={setmembers} />}/>
+      <Route path="/member" element={<MemberTable  members={members
+      }/>}/>
+    
+    </Routes>
+    </>
   );
 }
 
